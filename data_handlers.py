72a3430eae96194d7ed986159aa7dded617b1852
Youssef Farag <youssef.m.farag@gmail.com>
2020-04-10 15:11:10 -0700

    Greatness is born.

---
+++ data_handlers.py
@@ -0,0 +1,40 @@
+from torch.utils.data import Dataset, DataLoader
+
+
+class CovidDatasetTrain(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, imgs, labels):
+        self.imgs = imgs
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.imgs[idx], self.labels[idx]
+
+
+class CovidDatasetTest(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, imgs):
+        self.imgs = imgs
+
+    def __len__(self):
+        return self.imgs.shape[0]
+
+    def __getitem__(self, idx):
+        return self.imgs[idx]
+
+
+def make_data_loaders(train_imgs, train_labels, test_imgs):
+    train_dataset = CovidDatasetTrain(train_imgs[:50], train_labels[:50])
+    val_dataset = CovidDatasetTrain(train_imgs[50:], train_labels[50:])
+    test_dataset = CovidDatasetTest(test_imgs)
+
+    return {
+        "train": DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=1),
+        "val": DataLoader(val_dataset, batch_size=1, shuffle=True, num_workers=1),
+        "test": DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1),
+    }
---
+++ train.py
@@ -0,0 +1,73 @@
+from __future__ import print_function, division
+import torch
+import pretrainedmodels
+import torch.nn as nn
+from data_handlers import *
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision.transforms as tvt
+import pickle
+
+
+def prep_data():
+    test_imgs = pickle.load(open("../data/test_images_512.pk", 'rb'), encoding='bytes')
+    train_imgs = pickle.load(open("../data/train_images_512.pk", 'rb'), encoding='bytes')
+    train_labels = pickle.load(open("../data/train_labels_512.pk", 'rb'), encoding='bytes')
+
+    data_loaders = make_data_loaders(train_imgs, train_labels, test_imgs)
+    return data_loaders
+
+
+if __name__=='__main__':
+
+    data_loaders = prep_data()
+
+    model_name = 'resnet18'
+    model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet')
+    model.last_linear = nn.Linear(model.last_linear.in_features, 1)
+    model.cuda()
+
+    bigfella = optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.BCEWithLogitsLoss(weight=torch.tensor([0.3]))
+    correct = 0
+
+    with torch.no_grad():
+        model.eval()
+        for sample, label in data_loaders['val']:
+            preds = nn.Softmax(dim=1)(model(sample.cuda()))
+            if np.rint(preds.cpu().numpy()) == label.numpy():
+                correct += 1
+
+    print('Accuracy before training is {}, {} correct'.format(correct/len(data_loaders['val']), correct))
+    model.train()
+
+    for epoch in range(15):
+        preds = []
+        labels = []
+
+        bigfella.zero_grad()
+        for sample, label in data_loaders["train"]:
+            sample = tvt.ToTensor()(tvt.RandomCrop(350)(tvt.ToPILImage()(sample.squeeze(0)))).unsqueeze(0)
+            pred = model(sample.cuda())
+            preds.append(pred.cpu().squeeze(0))
+            labels.append(label)
+            if label == 0:
+                print(nn.Sigmoid()(pred).item())
+
+        loss = criterion(torch.stack(preds, dim=1), torch.stack(labels, dim=1).float())
+        print('Loss is {}'.format(loss.item()))
+        loss.backward()
+        bigfella.step()
+
+    correct = 0
+
+    with torch.no_grad():
+        model.eval()
+        for sample, label in data_loaders['val']:
+            preds = nn.Softmax(dim=1)(model(sample.cuda()))
+            if np.rint(preds.cpu().numpy()) == label.numpy():
+                correct += 1
+
+    print('Accuracy after training is {}, {} correct'.format(correct/len(data_loaders['val']), correct))
+    model.train()
